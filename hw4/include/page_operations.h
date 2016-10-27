88dc5607ee4d0fa1f232e8be4c2aa8ab2a2216f0
Ryan <ryan.zaeni@sjsu.edu>
2016-10-27 12:31:26 -0700

    Added find_freepages header

--- hw4/include/page_operations.h
+++ hw4/include/page_operations.h
@@ -6,3 +6,6 @@ void generate_pages(page** list);
 
 
 void print_pagesLL(page* llist);
+
+//Returns true if we find 4 free pages and we mark those 4 pages as not available. 
+bool find_4FreePages(page* llist);
