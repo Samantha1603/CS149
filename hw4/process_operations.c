a54cff7fea103d01b92b9c30bfdd6274ceb2bec9
benj-G <benjamin.gottheil@sjsu.edu>
2016-10-26 18:22:22 -0700

    not even close, but started merge sort

--- hw4/process_operations.c
+++ hw4/process_operations.c
@@ -2,6 +2,7 @@
 #include <stdlib.h>
 #include <time.h>
 #include "shared.h"
+#include "process_operations.h"
 
 void generate_processes(process** list){
 	process* b_list;
@@ -62,6 +63,8 @@ void sort_pll(process* list){
 	}
 	hi = &*(hi_prev->next);
 
+	//initial swap, tests if the first and last members are incorrect. Special cases because
+	//there is no previous to the beginning and the beginning of the list, b_list, must be set to the new value
 	printf("Print hi and prev: HI - %c PREV: %c\n", hi->name[0], hi_prev->name[0]);
 	if(hi->arrival_time < lo->arrival_time){
 		printf("Test HI arrival: %d LO arrival: %d\n", hi->arrival_time, lo->arrival_time);
@@ -74,24 +77,51 @@ void sort_pll(process* list){
 		hi->next = &*p;
 		printf("hi->next set to temp\n");
 		if(lo_prev != NULL) lo_prev->next = &*hi;
-		head = hi;
-	}	
 
-	// All pointers should be set up start sorting
-	/*for(int x = 0; x < counter; x++){
-		if(hi->arrival_time < lo->arrival_time){
-			printf("Test HI arrival: %d LO arrival: %d\n", hi->arrival_time, lo->arrival_time);
-			hi_prev->next = &*lo;
-			printf("hi_prev->next set to lo\n");
-			p = &*(lo->next);
-			printf("temp set to lo->next\n");
-			lo->next = &*(hi->next);
-			printf("lo->next set to hi->next\n");
-			hi->next = &*p;
-			printf("hi->next set to temp\n");
-			if(lo_prev != NULL) lo_prev->next = &*hi;
+		//switching pointer names. After this switch, we can count on lo, being the element with a lower arrival time
+		// than the hi pointer
+		p = hi;
+		hi = lo;
+		lo = p;
+		b_list = lo;
+	}	
+	// All pointers should be set up start sorting. We will be comparing based on next.
+	//  This eliminates the need for a "previous" pointer
+	sort_pll_r(&b_list, 0, counter / 2);
+	sort_pll_r(&b_list, (counter / 2) + 1, counter);
+	print_ll(b_list);
+	return;
+}
+
+void sort_pll_r(process** list, int lo, int hi){
+	if(hi - lo <= 1) return;
+	if(hi - lo == 2){
+		process** p1 = &(*list);
+		process** p2 = &(*list);
+		process* p3;
+		for(int x = 0; x < lo - 1; x++){
+			*p1 = &(*(*p1)->next);
 		}
-	}*/
-	print_ll(head);
+		for(int x= 0; x < hi - 1; x++){
+			*p2 = &(*(*p2)->next);
+		}
+		if((*p1)->next->arrival_time > (*p2)->next->arrival_time){
+			printf("swapping: ");
+			print_process(*((*p2)->next));
+			print_process(*((*p1)->next));
+
+			p3 = (*p2)->next;
+			(*p2)->next = (*p1)->next;
+			(*p1)->next = p3->next;
+
+			p3 = (*p1)->next->next;
+			(*p1)->next->next = (*p2)->next->next;
+			(*p2)->next->next = p3;
+		}
+		return;
+	}
+	printf("Hi: %d, Lo: %d", hi, lo);
+	sort_pll_r(&(*list), lo, (hi-lo) / 2);
+	sort_pll_r(&(*list), ((hi-lo) / 2) + 1, hi);
 	return;
 }
