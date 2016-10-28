ea4837aaee744cc7bf2de75a2727e4b59072b8e2
Ryan <ryan.zaeni@sjsu.edu>
2016-10-27 19:32:53 -0700

    Added function call to create page linked list

--- hw4/main.c
+++ hw4/main.c
@@ -1,8 +1,10 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
+#include <stdbool.h>
 #include "shared.h"
 #include "process_operations.h"
+
 /// Amount of memory in MB
 #define MEMORY 100
 
@@ -13,7 +15,9 @@ int main(){
 	srand(time(NULL));
 	// Generate Processes
 	process* llist;
+	page* linkedlist;
 	generate_processes(&llist);
+	generate_pageList(&linkedlist);
 	print_ll(llist);
 	printf("\n\nAfter sort\n\n");
 	sort_pll(llist);
