a435b12e28741389340ff03add3255a3ff78faa0
Ryan <ryan.zaeni@sjsu.edu>
2016-10-27 23:24:33 -0700

    Now creates and prints initial page list

--- hw4/main.c
+++ hw4/main.c
@@ -4,6 +4,7 @@
 #include <stdbool.h>
 #include "shared.h"
 #include "process_operations.h"
+#include "page_operations.h"
 
 /// Amount of memory in MB
 #define MEMORY 100
@@ -15,9 +16,12 @@ int main(){
 	srand(time(NULL));
 	// Generate Processes
 	process* llist;
-	page* linkedlist;
+	page* pagelinkedlist;
 	generate_processes(&llist);
-	generate_pageList(&linkedlist);
+	generate_pageList(&pagelinkedlist);
+	printf("\nHere is our initial page list\n");
+	print_pageLinkedList(pagelinkedlist);
+	printf("\nEND OF PAGES\n");
 	print_ll(llist);
 	printf("\n\nAfter sort\n\n");
 	sort_pll(llist);
