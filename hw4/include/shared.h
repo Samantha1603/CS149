7408872efc6d2cb41aadbc2edec227acfcdf8f2a
benj-G <benjamin.gottheil@sjsu.edu>
2016-10-29 15:11:45 -0700

    created connections between pages and procceses

--- hw4/include/shared.h
+++ hw4/include/shared.h
@@ -11,6 +11,7 @@ struct process{
 	int page_size; // Can be 5, 11, 17, and 31
 	int arrival_time; // 
 	int completion_time; // Can be 1, 2, 3, 4, or 5
+	struct page* pagesowned[4]; //4 page pointers to the 4 possible pages in memory
 	struct process* next; // makes possible to create linked list
 };
 
@@ -20,6 +21,7 @@ typedef struct process process;
 struct page{
 	int status; //All 100 pages are initially set to available which is 0. 
 					//Set status to 1 for not available page.
+	struct process* process_owner; //pointer to the process owning this page
 	struct page* next; 
 };
 
