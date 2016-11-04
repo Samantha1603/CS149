d4654647de5e98c8dfeb22a6be1b78d338391522
benj-G <benjamin.gottheil@sjsu.edu>
2016-11-04 01:57:47 -0700

    added ryan's funciton for when replacement is needed

--- hw4/page_operations.c
+++ hw4/page_operations.c
@@ -69,7 +69,7 @@ void removeAPage(page** list, int nodeIndex)
 			temp = temp->next;
 			if (temp == NULL) return;
 		}
-		temp->status = -1;
+		temp->status = 0;
 		return;
 }
 
@@ -88,6 +88,8 @@ void addPageToMemory(page** list, process* p1, int inMemoryTime, int pageNumber)
 	for(int x = 0; x < p1->num_page_in_freelist ; x++){
 		if(p1->pagesowned[x].pageNumber == pageNumber) return;
 	}
+	if(!find_4FreePages) return; // this is where page replacement algorithms should begin
+	//They should decide what to do with pages once memory fills
 	// End Check
 	page* current;
 	page* temp;
