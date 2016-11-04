ae41b6c317b3bb49ff36e2642888f245d330cbfd
benj-G <benjamin.gottheil@sjsu.edu>
2016-11-03 23:57:18 -0700

    remade this function. deleted removal of nodes. free nodes are now flagged with a -1 status. Additional changes, such as no need to pass in which page to insert, function does it by itself

--- hw4/page_operations.c
+++ hw4/page_operations.c
@@ -60,60 +60,56 @@ bool find_4FreePages(page* llist){
 // Remove the page at a given position
 void removeAPage(page** list, int nodeIndex) 
 {
-	page* prev;
-	page* temp;
+	page* temp = *list;
 
 	if (list == NULL || *list == NULL || nodeIndex < 0) return;
 
-	if (nodeIndex == 0) { // Remove head
-		temp = (*list)->next;
-		free(*list);// COULD CAUSE PROBLEMS. 9/10 WILL CAUSE PROBLEMS
-		*list = temp;
-
-	} else {
-		prev = *list;
-		temp = (*list)->next;
-
 		// Go to the node position 
 		for (int x = 1; x < nodeIndex; x++) {
-			prev = temp;
 			temp = temp->next;
 			if (temp == NULL) return;
 		}
-		prev->next= temp->next; // Skip over the node to be removed
-		free(temp); // Remove the node waiting for removal
+		temp->status = -1;
+		return;
+}
+
+page* findFreePage(page *list, int length){
+	page* traverse = list;
+	for(int x = 0; x < length; x++){
+		if(traverse->status == 0) return traverse;
 	}
+	return NULL;  //check for null if no free page is found
 }
 
 // Add page to first available position, starting from head node
-void addPageToMemory(page** list, page* pageToInsert, process* p1, int inMemoryTime, int pageNumber) 
+void addPageToMemory(page** list, process* p1, int inMemoryTime, int pageNumber) 
 {
 	page* current;
 	page* temp;
 	current = *list;
-
-	if (current->status == 0) { //Insert at head
-		temp = current;
-		*list = pageToInsert; // Change head to new page
-		pageToInsert->next = temp->next;
-	} else {
-		// Loop as long as current node is occupied 
-		while (current->next->status != 0) {
-			current = current->next;
-		}
-
-		temp = current->next->next;
-		current->next = pageToInsert;
-		pageToInsert->next = temp; 
+	page insert;
+	while(current->status == 1){
+		current = current->next;
 	}
+
+	insert.status=1;
+	insert.inMemoryTime = inMemoryTime;
+	insert.process_owner = p1;
+	insert.pageNumber = pageNumber;
+	/*
 	pageToInsert->status = 1; // change status to true when occupied
 	pageToInsert->inMemoryTime = inMemoryTime; // time page added to memory
 	pageToInsert->process_owner = p1;
 	pageToInsert->pageNumber = pageNumber;
-	p1->num_page_in_freelist++;
+	*/
 	for(int x = 0; x < p1->page_size; x++){
-		if(p1->pagesowned[x].isInMemory == false) p1->pagesowned[x] = *pageToInsert;
-		break;
+		if(p1->pagesowned[x].status != 1){
+			p1->pagesowned[x] = insert;
+			insert.next = current->next;
+			*current = insert;
+			p1->num_page_in_freelist++;
+			return;;
+		}
 	}
 	return;
 }
@@ -145,7 +141,7 @@ void print_pagesLL(page* llist) {
 	for(int x = 0; x < NUMBER_PAGES; x++){
 		if(head == NULL) break;
 		printf("Status: %d \t Page Number: %d", head->status, head->pageNumber);
-		if(head->status == true) printf(" Page Owner: %c\n", head->process_owner->name[0]);
+		if(head->status == 1) printf(" Page Owner: %c\n", head->process_owner->name[0]);
 		else printf(" Page Owner: Free\n");
 		head = head->next;
 	}
