229ef62968a31efdc2cce59684c11c1d9f5b19f3
Ryan <ryan.zaeni@sjsu.edu>
2016-11-07 00:20:06 -0800

    Added header for LFU

---
+++ hw4/include/LFU.h
@@ -0,0 +1,19 @@
+#include <stdbool.h>
+#ifndef LFU
+#define LFU
+
+
+void startLFU(process** processbyarrivial, page** pagellist);
+
+page* getLowFreqAndHighTimePage(page* pagelist);
+
+bool isPageAlreadyInMemory(process* p1, int pageNumber);
+
+bool isMemoryFull(page* llist);
+
+void print_pages(page* llist);
+
+void swapWithLowFreqAndHighTimePage(page** pagelist, process* p1, int inMemoryTime, int pageNumber, int frequency);
+
+
+#endif
