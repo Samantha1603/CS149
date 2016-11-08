03deae29b62c11d3afd648cef2f22db095967da8
Ryan <ryan.zaeni@sjsu.edu>
2016-11-08 13:27:39 -0800

    Headers

--- hw4/include/MFU.h
+++ hw4/include/MFU.h
@@ -2,9 +2,10 @@
 #ifndef MFU
 #define MFU
 
+
 void startMFU(process** prolist, page** pagelist);
 
-page* getHighFreqAndHighTimePage(page* pagelist);
+page* getHighFreqAndHighTimePage(page* pagelist, int* freqArray);
 
 bool isPageAlreadyInMemoryMFU(process* p1, int pageNumber);
 
@@ -12,12 +13,11 @@ bool isMemoryFullMFU(page* llist);
 
 void print_pagesMFU(page* llist);
 
-void swapWithHighFreqAndHighTimePage(page** pagelist, process* p1, int inMemoryTime, int pageNumber);
+void swapWithHighFreqAndHighTimePage(page** pagelist, process* p1, int inMemoryTime, int pageNumber, int* freqArray);
 
-void removePageFromAProcessArrayMFU(process* p1, page* highFreqPage);
+void removePageFromAProcessArrayMFU(process* p1, page* oldestPage);
 
-void removePageFromFreeListMFU(page** pagelist, char pageToRemove1, char pageToRemove2);
+void removePageFromFreeListMFU(page** pagelist, char pageToRemove1, char pageToRemove2, int* freqArray);
 
 void printStatsMFU(int hitCount, int missCount, int numOfProcessesDone);
-
 #endif
